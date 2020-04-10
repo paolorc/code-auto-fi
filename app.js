cfa3acbe32c6b741cb15834674bbb1c2b2a54098
Paolo Reyes <paolo.reyes.2911@gmail.com>
2020-04-09 19:01:46 -0500

    elimine un archivo y lineas de comandos

--- app.js
+++ app.js
@@ -16,10 +16,7 @@ app.use(compression());
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(compression());
-
+// esto es una broma
 mongoose.Promise = Promise;
 mongoServer.getConnectionString().then((mongoUri) => {
     const mongooseOpts = {
